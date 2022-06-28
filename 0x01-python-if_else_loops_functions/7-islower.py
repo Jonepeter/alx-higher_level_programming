d5bc2fb49ccc87a13deeb4e790e320050636f4ba
Petros <petros@mtu.edu.et>
2022-06-28 16:41:34 +0300

    is lower

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-def islower(str):
-    if ord(str) >= 97 and ord(str) <= 122:
+def islower(c):
+    if ord(c) >= 97 and ord(c) <= 122:
         return True
     else:
         return False
