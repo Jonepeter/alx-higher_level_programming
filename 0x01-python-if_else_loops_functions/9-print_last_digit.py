92024ea5a71d2a7a90d494019eba704a92a83d0c
Petros <petros@mtu.edu.et>
2022-06-28 17:19:51 +0300

    print last digit using python function

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    if number > 0:
+        last_digit = number % 10
+    else:
+        last_digit = (abs(number) % 10) * (-1)
+    print("{0}".format(last_digit))
