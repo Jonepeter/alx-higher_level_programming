7b52fb27c2096256101e5fd5b2e6af5307520113
Petros <petros@mtu.edu.et>
2022-06-30 22:09:44 +0300

    my calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,6 +3,15 @@ from sys import argv
 from calculator_1 import add, sub, mul, div
 
 if __name__ == '__main__':
+    '''
+        Handles basic operations
+        
+        Performs basic operations like addition, substraction,
+        multiplication and division between two numbers.
+        
+        The program will execute an operation between two numbers
+        selected by the operator sent to the program.
+                '''
     len_args = len(argv) - 1
     
     if len_args == 3:
