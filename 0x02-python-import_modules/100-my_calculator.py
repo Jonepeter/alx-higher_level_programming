ab00bce7b8b753d40f55e2ea954aa7f8681fe66e
Petros <petros@mtu.edu.et>
2022-06-30 18:32:06 +0300

    hidden discovery

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,3 @@
+from calculator_1 import add, sub, mul, div
+if __name__ == '__main__':
+    print("Testing  calculator")
---
+++ 0x02-python-import_modules/5-variable_load.py
@@ -0,0 +1,3 @@
+from add_0 import a
+if __name__ == '__main__':
+    print("{}".format(a))
