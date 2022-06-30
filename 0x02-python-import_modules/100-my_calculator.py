610fa6fc1e7583186b831e37c49e76c5c204bbf8
Petros <petros@mtu.edu.et>
2022-06-30 22:16:16 +0300

    my calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -9,19 +9,19 @@ if __name__ == '__main__':
         num1 = int(argv[1])
         num2 = int(argv[3])
         operator = argv[2]
-        if (operator == '+'):
+        if operator == '+':
             result = add(num1, num2)
             print("{:d} + {:d} = {:d}".format(num1, num2, result))
             exit(0)
-        elif (operator == '-'):
+        elif operator == '-':
             result = sub(num1, num2)
             print("{:d} - {:d} = {:d}".format(num1, num2, result))
             exit(0)
-        elif (operator == '*'):
+        elif operator == '*':
             result = mul(num1, num2)
             print("{:d} * {:d} = {:d}".format(num1, num2, result))
             exit(0)
-        elif (operator == '/'):
+        elif operator == '/':
             result = int(div(num1, num2))
             print("{:d} / {:d} = {:d}".format(num1, num2, result))
             exit(0)
