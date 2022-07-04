95451ade3eea4f04c913cc7e3800a40602010c88
Petros <petros@mtu.edu.et>
2022-07-04 15:40:46 +0300

    replace in list

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,13 +1,5 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    '''
-        Function that replaces an element of specific idx with new element
-        
-        If idx is negative, the function should not modify anything, 
-        and returns the original list
-        If idx is out of range (> of number of element in my_list), 
-        the function should not modify anything, and returns the original list
-    '''
     if idx < 0:
         return my_list
     elif idx >= len(my_list):
