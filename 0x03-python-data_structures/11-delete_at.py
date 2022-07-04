70f0046c310d1981bd0fe89ec751ad5fe0063004
Petros <petros@mtu.edu.et>
2022-07-04 18:11:29 +0300

    delete at

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,15 +1,8 @@
 #!/usr/bin/python3
-from re import M
-
-
 def delete_at(my_list=[], idx=0):
-    # if idx is negative or out of range, nothing change (returns the same list)
-    new_list = []
     leng = len(my_list) - 1
     if idx >= 0 and idx <= leng:
-        for i in my_list:
-            if i != my_list[idx]:
-                new_list.append(i)
-        return new_list
+        del my_list[idx]
+        return my_list
     else:
-        return new_list
+        return my_list
