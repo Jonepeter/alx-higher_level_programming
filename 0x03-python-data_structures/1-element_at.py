e8d72e061427c67e1ce78ebef552ad57ff36fa02
Petros <petros@mtu.edu.et>
2022-07-04 15:38:59 +0300

    element at

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,11 +1,5 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    '''
-        Function that returns the element at the given index
-        
-        If idx is negative, the function should return None
-        If idx is out of range (> of number of element in my_list), the function should return None
-    '''
     if idx < 0:
         return None
     elif idx > (len(my_list) - 1):
