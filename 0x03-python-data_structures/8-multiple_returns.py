411721cf9a15d5a0a98fe134f7aa85dc8737344f
Petros <petros@mtu.edu.et>
2022-07-04 16:15:56 +0300

    multiple return

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if sentence != " ":
+    if sentence != "":
         length = len(sentence)
         first_letter = sentence[0]
         return(length, first_letter)
     else:
-        return(0,None)
+        return(0, None)
