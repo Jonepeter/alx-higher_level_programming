b64cc824514f0336327951336d852594129c6fd3
Gebreyes <gebreyesis56@gmail.com>
2022-08-01 18:19:49 +0300

    Task 1

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Module.
+
+
+
+"""
+
+
+class MyList(list):
+    """
+    class
+    """
+    def print_sorted(self):
+        """
+        method prints sorted list.
+        """
+        print(sorted(self))
