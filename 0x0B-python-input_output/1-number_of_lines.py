5e9c85b2faad9e434752eb9bdf5f00f90b98ddd8
Gebreyes <gebreyesis56@gmail.com>
2022-08-02 21:07:14 +0300

    Number of lines

--- 0x0B-python-input_output/1-number_of_lines.py
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -1,15 +1,9 @@
 #!/usr/bin/python3
-"""Module.
-
-
-
-"""
+"""Module number_of_lines"""
 
 
 def number_of_lines(filename=""):
-    """
-    function.
-    """
+    """ function number_of_lines """
     x = 0
     with open(filename) as f:
         return len(list(f))
