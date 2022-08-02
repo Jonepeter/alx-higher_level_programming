641def73713c15c3c939c03a3ac69491eb9b3544
Gebreyes <gebreyesis56@gmail.com>
2022-08-02 22:08:47 +0300

    log parsing

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
 ''' Module Input Output'''
+import sys
 
 
 def print_stats(size, status_codes):
+    ''' Print statistics'''
     print("File size: {}".format(size))
     for key in sorted(status_codes):
         print("{}: {}".format(key, status_codes[key]))
 
-if __name__ == "__main__":
-    import sys
 
+if __name__ == "__main__":
+      
     size = 0
     status_codes = {}
     valid_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
