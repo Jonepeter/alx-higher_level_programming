d3abb121a94b216f6a30db96e5aa780801929b6b
Petros Abebe <87171491+Jonepeter@users.noreply.github.com>
2022-11-15 21:52:19 +0300

    Delete 0x14-javascript-web_scraping directory

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/node
-// prints the title of a starwars movie
-const request = require('request');
-
-request(`https://swapi-api.hbtn.io/api/films/${process.argv[2]}`, function (err, response, body) {
-  console.log(err || JSON.parse(body).title);
-});
